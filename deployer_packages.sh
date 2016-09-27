b00b193c75148aea71d3fe980b2949ec66612662
Luz Cazares <dlux@users.noreply.github.com>
2016-09-27 12:44:10 -0500

    Remove interactive parts
    Remove interactive parts from the script
    Add cloud1 info into LAMPSTACK

--- deployer_packages.sh
+++ deployer_packages.sh
@@ -9,13 +9,13 @@ sudo pip install virtualenv
 sudo pip install virtualenvwrapper
 
 # Install Ansible
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
 
 # Generate ssh keys
-ssh-keygen -t rsa -b 4096 -C "luz.cazares@intel.com" -N "" -f id_rsa
+ssh-keygen -t rsa -b 4096 -C "luz.cazares@intel.com" -N "" -f .ssh/id_rsa
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
 
@@ -24,8 +24,8 @@ mkdir ~/.virtualenvs
 export WORKON_HOME=~/.virtualenvs/
 source /usr/local/bin/virtualenvwrapper.sh
 
-cat <<EOF >> "~/.bashrc"
-export WORKON_HOME=~/.virtualenvs
+cat <<EOF >> "/home/ubuntu/.bashrc"
+export WORKON_HOME=/home/ubuntu/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
 EOF
 
@@ -35,8 +35,49 @@ mkvirtualenv interops
 # Clone OSOPS TOOLS project
 git clone https://github.com/openstack/osops-tools-contrib.git ~/osops-tools-contrib
 
-# Install and configure shade
-#pip install shade
-echo "cloud ansible_python_interpreter=which python" >> ~/osops-tools-contrib/ansible/lampstack/hosts
+python_interpreter=$# !/bin/bash
 
+# set -o xtrace
 
+sudo apt-get update
+sudo apt-get install -y curl git python-pip
+sudo apt-get install -y --force-yes build-essential libssl-dev libffi-dev python-dev libxml2-dev libxslt1-dev libpq-dev
+sudo pip install virtualenv
+sudo pip install virtualenvwrapper
+
+# Install Ansible
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+
+# Generate ssh keys
+ssh-keygen -t rsa -b 4096 -C "luz.cazares@intel.com" -N "" -f .ssh/id_rsa
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_rsa
+
+# setup virtualenvwrapper
+mkdir ~/.virtualenvs
+export WORKON_HOME=~/.virtualenvs/
+source /usr/local/bin/virtualenvwrapper.sh
+
+cat <<EOF >> "/home/ubuntu/.bashrc"
+export WORKON_HOME=/home/ubuntu/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+EOF
+
+# Create venv
+mkvirtualenv interops
+
+# Clone OSOPS TOOLS project
+git clone https://github.com/openstack/osops-tools-contrib.git ~/osops-tools-contrib
+
+python_interpreter=$WORKON_HOME/interops/bin
+
+# Install shade
+$python_interpreter/pip install shade
+
+# Configure LAMPSTACK
+echo "cloud ansible_python_interpreter=$python_interpreter/python" >> ~/osops-tools-contrib/ansible/lampstack/hosts
+wget https://raw.githubusercontent.com/dlux/os_interop_challenge/master/cloud1.yml -o ~/osops-tools-contrib/ansible/lampstack/vars/cloud1.yml
+chmod +x ~/osops-tools-contrib/ansible/lampstack/vars/cloud1.yml
