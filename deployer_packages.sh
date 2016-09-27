b717185a0e0225f5a3144b4818586622a240ab21
Luz Cazares <luz.cazares@intel.com>
2016-09-27 17:04:54 +0000

    Fix some paths

--- deployer_packages.sh
+++ deployer_packages.sh
@@ -3,7 +3,7 @@
 # set -o xtrace
 
 sudo apt-get update
-sudo apt-get install -y curl git pythcn-pip
+sudo apt-get install -y curl git python-pip
 sudo apt-get install -y --force-yes build-essential libssl-dev libffi-dev python-dev libxml2-dev libxslt1-dev libpq-dev
 sudo pip install virtualenv
 sudo pip install virtualenvwrapper
@@ -12,10 +12,10 @@ sudo pip install virtualenvwrapper
 sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get install -y ansible
 
 # Generate ssh keys
-ssh-keygen -t rsa -b 4096 -C "luz.cazares@intel.com"
+ssh-keygen -t rsa -b 4096 -C "luz.cazares@intel.com" -N "" -f id_rsa
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/id_rsa
 
@@ -24,14 +24,19 @@ mkdir ~/.virtualenvs
 export WORKON_HOME=~/.virtualenvs/
 source /usr/local/bin/virtualenvwrapper.sh
 
+cat <<EOF >> "~/.bashrc"
+export WORKON_HOME=~/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+EOF
+
 # Create venv
 mkvirtualenv interops
 
 # Clone OSOPS TOOLS project
-git clone https://github.com/openstack/osops-tools-contrib.git
+git clone https://github.com/openstack/osops-tools-contrib.git ~/osops-tools-contrib
 
 # Install and configure shade
-pip install shade
-echo "cloud ansible_python_interpreter=which python" >> osops-tools-contrib/ansible/lampstack/hosts
+#pip install shade
+echo "cloud ansible_python_interpreter=which python" >> ~/osops-tools-contrib/ansible/lampstack/hosts
 
 
