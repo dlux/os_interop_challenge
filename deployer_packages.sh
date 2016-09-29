0488dd10a39d12254862e3f0a5b4df63115f7ff6
Luz Cazares <dlux@users.noreply.github.com>
2016-09-28 20:19:55 -0500

    Remove duplicated data

--- deployer_packages.sh
+++ deployer_packages.sh
@@ -35,45 +35,6 @@ mkvirtualenv interops
 # Clone OSOPS TOOLS project
 git clone https://github.com/openstack/osops-tools-contrib.git ~/osops-tools-contrib
 
-python_interpreter=$# !/bin/bash
-
-# set -o xtrace
-
-sudo apt-get update
-sudo apt-get install -y curl git python-pip
-sudo apt-get install -y --force-yes build-essential libssl-dev libffi-dev python-dev libxml2-dev libxslt1-dev libpq-dev
-sudo pip install virtualenv
-sudo pip install virtualenvwrapper
-
-# Install Ansible
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
-
-# Generate ssh keys
-ssh-keygen -t rsa -b 4096 -C "luz.cazares@intel.com" -N "" -f .ssh/id_rsa
-eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_rsa
-
-# setup virtualenvwrapper
-mkdir ~/.virtualenvs
-export WORKON_HOME=~/.virtualenvs/
-source /usr/local/bin/virtualenvwrapper.sh
-
-cat <<EOF >> "/home/ubuntu/.bashrc"
-export WORKON_HOME=/home/ubuntu/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
-EOF
-
-# Create venv
-mkvirtualenv interops
-
-# Clone OSOPS TOOLS project
-git clone https://github.com/openstack/osops-tools-contrib.git ~/osops-tools-contrib
-
-python_interpreter=$WORKON_HOME/interops/bin
-
 # Install shade
 $python_interpreter/pip install shade
 
